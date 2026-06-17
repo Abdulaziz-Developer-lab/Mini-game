a2bb46fc55f02cbc6a8473062325aba8db839065
Abdulaziz-Developer-lab <xurshidaa399@gmail.com>
2026-06-17 14:31:55 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
 const app = express();
+app.use(helmet());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
